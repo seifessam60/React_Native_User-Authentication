ccb438844b1efccc0393cf65c7120d80fd0f70ef
seifessam60 <seifessam60@gmail>
2025-05-19 22:18:06 +0300

    Creating New Users

---
+++ screens/SignupScreen.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+
+function SignupScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  }
+  if (isAuthenticating) {
+    return <LoadingOverlay message={"Creating user..."} />;
+  }
+  return <AuthContent onAuthenticate={signupHandler} />;
+}
+
+export default SignupScreen;
